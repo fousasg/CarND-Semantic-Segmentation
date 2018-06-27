daeabe4212de148da98af3b7e4203e3be8fdec89
fousasg <odisseas_mpd@hotmail.com>
2018-06-27 18:45:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -133,7 +133,7 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     logits = tf.reshape(nn_last_layer, (-1, num_classes))
     
     ## applying softmax
-    cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits, correct_label))
+    cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=correct_label))
     
     # Usinf Adam as suggested
     optimizer = tf.train.AdamOptimizer(learning_rate= learning_rate)
